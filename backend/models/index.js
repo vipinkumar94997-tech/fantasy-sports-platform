c22986d15f847fa0b9da9ebc5fb77ba482de45bf
vipin <vipinkumar94997@gmail.com>
2026-06-05 15:58:25 +0530

    Otp.js → OTP.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -10,7 +10,7 @@ import Leaderboard from "./Leaderboard.js";
 import Notification from "./Notification.js";
 import PromoCode from "./PromoCode.js";
 import Referral from "./Referral.js";
-import Otp from "./Otp.JS";
+import OTP from "./OTP.JS/";
 import Team from "./Team.js";
 import Match from "./Match.js";
 
