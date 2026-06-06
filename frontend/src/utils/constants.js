58df27ee2768d9e01ae2a129e96bd99fb43c929f
vipin <vipinkumar94997@gmail.com>
2026-06-06 09:22:55 +0530

    updated production api url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,7 +1,10 @@
 export const API_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+  import.meta.env.VITE_API_URL ||
+  "https://fantasy-sports-platform-6.onrender.com/api";
+
 export const SOCKET_URL =
-  import.meta.env.VITE_SOCKET_URL || "http://localhost:5000";
+  import.meta.env.VITE_SOCKET_URL ||
+  "https://fantasy-sports-platform-6.onrender.com";
 
 export const RESTRICTED_STATES = [
   "Assam",
