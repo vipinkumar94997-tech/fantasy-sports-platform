ab7c00b549e3d95e5fbdd9baf4ecc72290ff5b2d
vipin <vipinkumar94997@gmail.com>
2026-06-09 11:22:22 +0530

    fix db config

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,18 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     port: Number(process.env.DB_PORT),
+//     dialect: "mysql",
+//     logging: false,
+//   },
+// );
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -11,6 +23,14 @@ const sequelize = new Sequelize(
     port: Number(process.env.DB_PORT),
     dialect: "mysql",
     logging: false,
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+      connectTimeout: 60000,
+    },
   },
 );
 
